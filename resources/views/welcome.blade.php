f90a1161e049837809d74d9a66535afb51bdf57e
simranjeet <singh2018developer@gmail.com>
2018-07-15 16:22:09 +0530

    dummy practise
    not for use just dummy practise

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Landon App V. {{ $version }}
                 </div>
 
                 <div class="links">
--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,21 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return '<h3>Landon hotel App</h3>';
+    //return view('welcome');
 });
+
+Route::get('/about', function () {
+    $response_arr = [];
+    $response_arr['author'] = 'BP';
+    $response_arr['version'] = '0.1.1';
+
+    return $response_arr;
+    
+   
+    // return '<h3>About</h3>';
+    //return view('welcome');
+});
+
+
+Route::get('/di', 'ClientController@di');
